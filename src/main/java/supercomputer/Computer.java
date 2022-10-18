4dc4c566a0d3a136af1f2bc98aa9cc0cdfb96fb0
Alexandre Charpentier <alexandre.charpentier2098@gmail.com>
2022-10-18 16:26:06 +0200

    Ajout attribut processor

--- src/main/java/supercomputer/Computer.java
+++ src/main/java/supercomputer/Computer.java
@@ -1,5 +1,16 @@
 package supercomputer;
 
 public class Computer {
+	
+	private int processor;
+
+	public int getProcessor() {
+		return processor;
+	}
+
+	public void setProcessor(int processor) {
+		this.processor = processor;
+	}
+	
 
 }
